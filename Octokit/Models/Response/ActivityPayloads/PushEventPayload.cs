mitchcapper/octokit.net

commit 7b3abda71129b3db504e8d8223a498064a251591

[feat]: Adding missing properties to PushEventPayload (#2795)

Octokit/Models/Response/ActivityPayloads/PushEventPayload.cs
@@ -6,6 +6,9 @@ namespace Octokit
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class PushEventPayload : ActivityPayload
     {
+        public int PushId { get; private set; }
+        public int DistinctSize { get; private set; }
+        public string Before { get; private set; }
         public string Head { get; private set; }
         public string Ref { get; private set; }
         public int Size { get; private set; }

Octokit.Tests/Clients/EventsClientTests.cs
@@ -6,6 +6,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using NSubstitute;
 using Octokit.Internal;
+using Octokit.Tests.Helpers;
 using Xunit;
 
 using static Octokit.Internal.TestSetup;
@@ -880,6 +881,9 @@ namespace Octokit.Tests.Clients
                         head = "head",
                         @ref = "ref",
                         size = 1337,
+                        before = "before",
+                        distinct_size = 1337,
+                        push_id = 1337,
                         commits = new []
                         {
                             new
@@ -902,6 +906,9 @@ namespace Octokit.Tests.Clients
             Assert.NotNull(payload.Commits);
             Assert.Equal(1, payload.Commits.Count);
             Assert.Equal("message", payload.Commits.FirstOrDefault().Message);
+            Assert.Equal("before", payload.Before);
+            Assert.Equal(1337, payload.DistinctSize);
+            Assert.Equal(1337, payload.PushId);
         }
 
         [Fact]

commit 62a1e9ae81f774dad94b4525b316b9a68531a57d

Fixes PushId datatype to not overflow (fix user activity exception)
Bug introduced in #2795 Closes #2822

Octokit/Models/Response/ActivityPayloads/PushEventPayload.cs
@@ -6,7 +6,7 @@ namespace Octokit
     [DebuggerDisplay("{DebuggerDisplay,nq}")]
     public class PushEventPayload : ActivityPayload
     {
-        public int PushId { get; private set; }
+        public long PushId { get; private set; }
         public int DistinctSize { get; private set; }
         public string Before { get; private set; }
         public string Head { get; private set; }